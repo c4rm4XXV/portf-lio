355472adebc4f3fcdcb04fda09c83e1e4818b844
Miguel Ângelo Chaves <45701585+c4rm4XXV@users.noreply.github.com>
2024-07-10 23:06:42 -0300

    Updrade Apresentação translete e Skill's img ajustes

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,21 +1,51 @@
 import style from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useState, useEffect } from 'react'
+function Presentation() {
+    const [text, setText] = useState('')
+    const toRotate = ['Miguel Ângelo', "Desenvolvedor Full-Stack", "Aux. de T.I"]
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const period = 2000;
+    const [delta, setDelta] = useState(2000)
 
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        })
+        return () => { clearInterval(ticker) }
+    }, [text])
 
-function Presentation(){
-    return(
+    const toType = () => {
+        let i = loop % toRotate.length
+        let fullText = toRotate[i]
+        let updatedText = isDeleting? fullText.substring(0,text.length-1): fullText.substring(0,text.length+1)
+        
+        
+        setText(updatedText);
+
+        if(!isDeleting&&updatedText===fullText){
+            setIsDeleting(true);
+            setDelta(period)
+        } else if(isDeleting&&updatedText===''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1)
+        }
+    }
+    return (
         <div id="Presentation" className={style.presentation}>
             <h4><storage>Bem-Vindo ao meu Portfólio</storage></h4>
 
-            <h1>Olá, eu sou Miguel Ângelo</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
-                Sou um apaixonado por tecnologia e soluções inovadoras.<br/>
-                Como Desenvolvedor Full-Stack, eu tenho o compromisso de resolver<br/> 
-                problemas complexos e ampliar meu leque de conhecimentos e projetos.<br/>
-                Meus projetos já geraram horas de empeinho e dedicação<br/>
+                Sou um apaixonado por tecnologia e soluções inovadoras.<br />
+                Como Desenvolvedor Full-Stack, eu tenho o compromisso de resolver<br />
+                problemas complexos e ampliar meu leque de conhecimentos e projetos.<br />
+                Meus projetos já proporcionaram horas de empeinho e dedicação<br />
                 estou sempre em busca de novos desafios para superar.
-            </p>            
-            <ButtonA link='https://github.com/c4rm4XXV' text='Conecte-se comigo'/>
+            </p>
+            <ButtonA link='https://github.com/c4rm4XXV' text='Conecte-se comigo' />
         </div>
     )
 }
